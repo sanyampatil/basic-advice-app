c42736bc28b18958c50d78fc40593fb94b69c3a5
sanyam <patilsanyam869@gmail.com>
2023-12-16 17:57:15 +0530

    reintialize repo

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App () {
+  return(
+    <>
+
+    <h1>
+
+    <h1>Advice app</h1>
+
+    </h1>
+  </>
+  
+  ) 
+}
+
+export default App
