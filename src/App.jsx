3c36e9696cb2dc1090565e7119b25a81176ccde7
sanyam <patilsanyam869@gmail.com>
2023-12-20 16:17:17 +0530

    added functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,49 @@
 import { useEffect, useState } from 'react'
-import './App.css'
-
+1
 function App () {
-  return(
+  const [data, setdata] = useState()
+  const [id, setid] = useState(1)
+  const [advice, setadvice] = useState("  zal ka jevan")
+
+  0
+0
+
+   function adviceRun () {
+    try {
+       fetch('https://api.adviceslip.com/advice')
+        .then(res => {
+          return res.json()
+        })
+        .then(data1 => {
+          setdata(data1)
+
+          setid(data1.slip.id)
+          setadvice(data.slip.advice)
+
+        })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
+  function handleClick () {
+    adviceRun()
+    console.log("hiii")
+  }
+  useEffect(() => {
+     adviceRun()
+  }, [adviceRun,  handleClick])
+
+
+  return (
     <>
+      <h1>Advice app{advice}</h1>
+      <h2>id is{id}</h2>
 
-    <h1>
-
-    <h1>Advice app</h1>
-
-    </h1>
-  </>
-  
-  ) 
+      <button onClick={handleClick}> click to advice</button>
+    </>
+  )
 }
 
 export default App
