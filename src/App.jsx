1bea7cb72fbb05e311af697f4685a450e56ef386
sanyam <patilsanyam869@gmail.com>
2023-12-30 23:29:06 +0530

    updated design

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,48 @@
 import { useEffect, useState } from 'react'
-1
+import axios from 'axios'
 function App () {
   const [data, setdata] = useState()
   const [id, setid] = useState(1)
   const [advice, setadvice] = useState('  zal ka jevan')
+  const [isloading, setisloading] = useState(true)
 
   0
   0
 
-  function adviceRun () {
-    try {
-      fetch('https://api.adviceslip.com/advice')
-        .then(res => {
-          return res.json()
-        })
-        .then(data1 => {
-          setdata(data1)
-
-          setid(data1.slip.id)
-          setadvice(data.slip.advice)
-        })
-    } catch (error) {
-      console.log(error)
-    }
+  async function adviceRun () {
+    const res = await axios.get('https://api.adviceslip.com/advice')
+    const Getdata = res.data.slip
+    console.log(Getdata)
+    setadvice(Getdata.advice)
+    setid(Getdata.id)
+    setisloading(false)
   }
 
   function handleClick () {
     adviceRun()
-    console.log('hiii')
   }
   useEffect(() => {
     adviceRun()
-  }, [])
-  console.log(setadvice)
+  }, [handleClick, setadvice, setid, setdata])
+
   return (
     <>
-      <div className=''>
-        <div className=''>
-          <h1 className=' text-2xl'> Advice:- {advice}</h1>
-          <h2>id:-{id}</h2>
+      <div className=' bg-blue-950 text-white w-full h-screen flex items-center justify-center  '>
+        <div className=' bg-blue-900 w-[50%] h-[50%] rounded-xl'>
+          <div className='flex flex-col items-center justify-center mt-12'>
+            <h1 className=' text-center font-bold text-xl'>
+              {' '}
+              {isloading ? 'loading....' : advice}
+            </h1>
 
-          <button
-            className=' text-2xl  bg-blue-200  border  border-black-500'
-            onClick={handleClick}
-          >
-            {' '}
-            click to advice
-          </button>
+            <button
+              className=' bg-blue-100  p-2 mt-10 rounded-xl font-bold text-blue-950 border hover:bg-slate-100  border-black-500'
+              onClick={handleClick}
+            >
+              {/* {' '} */}
+              click to advice
+            </button>
+          </div>
         </div>
       </div>
     </>
