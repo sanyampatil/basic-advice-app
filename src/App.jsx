739e749b02690755e1d86fad90904f6c6ddceacc
sanyam <patilsanyam869@gmail.com>
2024-01-01 17:22:45 +0530

    commit for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -32,20 +32,23 @@ function App () {
   return (
     <>
       <div className=' bg-blue-950 text-white w-full h-screen flex items-center justify-center  '>
-        <div className=' bg-blue-900 w-[50%] h-[50%] rounded-xl'>
+        <div className=' bg-blue-900 w-[70%] h-[70%] rounded-xl'>
           <div className='flex flex-col items-center justify-center mt-[15%]'>
-            <h1 className=' text-center font-bold text-xl'>
+            <h1 className='  text-center font-bold text-xl'>
               {' '}
               {(isloading) ? 'loading...' : advice}
             </h1>
+            <div className='mt-[12%]'>
+
 
             <button
-              className=' bg-blue-100  p-2 mt-[10%] rounded-xl font-bold text-blue-950 border hover:bg-slate-100  border-black-500'
+              className=' bg-blue-100  p-2  rounded-xl font-bold text-blue-950 border hover:bg-slate-100  border-black-500'
               onClick={handleClick}
             >
               {/* {' '} */}
               click to advice
             </button>
+            </div>
           </div>
         </div>
       </div>
